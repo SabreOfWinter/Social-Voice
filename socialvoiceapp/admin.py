96d53b6ceff59f385c6164dc384e9c5da82d4b55
SabreOfWinter <steadmanbailey@gmail.com>
2021-04-09 03:36:30 +0100

    Registered Thread, Audio messages models to Admin

--- socialvoiceapp/admin.py
+++ socialvoiceapp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Profile, Country, City 
+from .models import Profile, Country, City, AudioMessage, Thread, ThreadMessage
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Country)
 admin.site.register(City)
+admin.site.register(AudioMessage)
+admin.site.register(Thread)
+admin.site.register(ThreadMessage)
