d65c989cdfd8b98d0811bf0e944a2059108f8ca8
SabreOfWinter <steadmanbailey@gmail.com>
2021-04-12 21:27:06 +0100

    Import validators from validation file

--- socialvoiceapp/models.py
+++ socialvoiceapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from .validators import validate_file_extension
 # Create your models here.
 
 class Country(models.Model):
